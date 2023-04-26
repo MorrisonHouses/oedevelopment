b11092ae5ba8cc1c58674db1636d343ebc062836
MorrisonHouses <131913065+MorrisonHouses@users.noreply.github.com>
2023-04-26 15:58:24 -0600

    open project

---
+++ OEWebApplication/OEWebApplication/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var wshNetwork = new ActiveXObject("WScript.Network");
+var username = wshNetwork.username;
+var computername = wshNetwork.computername;
+var userdomain = wshNetwork.userdomain;
+document.getElementById("userName").innerHTML = username;
---
+++ OEWebApplication/OEWebData/UserFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace OEWebData
+{
+    public class UserFunctions
+    {
+        public void UserName()
+        {
+            IntPtr accountToken = WindowsIdentity.GetCurrent().Token;
+            Console.WriteLine("Token number is: " + accountToken.ToString());
+        }
+
+
+    }
+}
